8958e68c9422234baa8d94f4a9d74482a8d91a7b
Adawesome <adam.ccmpbll@gmail.com>
2018-03-04 16:50:55 -0800

    Conflict Checking Commit

---
+++ VRcade/Assets/LoadSceneOnClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ VRcade/Assets/TouchTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchTrigger : MonoBehaviour {
+    public EventSystem eventSys;
+    public GameObject selectedButton;
+    private bool buttonActive;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    void onTriggerEnter(Collider obj)
+    {
+        Debug.Log("Hmdah hmdah ultra omega tayay hmdah");
+        if (obj.tag == "GameController" && buttonActive == false)
+        {
+            eventSys.SetSelectedGameObject(selectedButton);
+            buttonActive = true;
+        }
+    }
+    private void OnDisable()
+    {
+        buttonActive = false;
+    }
+}
