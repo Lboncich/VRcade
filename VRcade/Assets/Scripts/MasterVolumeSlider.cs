04778f08f4e81f07f1f32c121a780597daf00138
Adawesome <adam.ccmpbll@gmail.com>
2018-03-14 01:19:21 -0700

    Tutorial Image Add
    Tutorial Image will appear and follow camera
    Wrote script [untested] that should kill the tutorial once the A-button
    is pressed on Touch

---
+++ VRcade/Assets/Scripts/MasterVolumeSlider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MasterVolumeSlider : MonoBehaviour {
+
+    public Slider volumeSlider;
+    public AudioSource masterVolume;
+	// Update is called once per frame
+	void Update () {
+        masterVolume.volume = volumeSlider.value;
+	}
+}
---
+++ VRcade/Assets/TerminateTutorial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRTK;
+public class TerminateTutorial : MonoBehaviour {
+
+    public VRTK_ControllerEvents rightController;
+    public Canvas tutCanvas;
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+	}
+    void terminateTutorial()
+    {
+        if(rightController.buttonOnePressed == true)
+        {
+            tutCanvas.gameObject.SetActive() = false;
+        }
+    }
+}
