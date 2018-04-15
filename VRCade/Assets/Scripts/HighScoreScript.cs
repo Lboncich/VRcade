997538e9eb4724f9caa545093f79727da666a5c4
Adawesome <adam.ccmpbll@gmail.com>
2018-04-15 01:44:19 -0700

    Rollback to old commit
    Not a clue how or why a rollback is needed. I didn't change anything but
    my own work that only ever existed on my local device and syncd every
    time before I started working...

--- VRCade/Assets/Scripts/HighScoreScript.cs
+++ VRCade/Assets/Scripts/HighScoreScript.cs
@@ -9,13 +9,6 @@ public class HighScoreScript : MonoBehaviour {
     public GameObject Name;
     public GameObject Rank;
 
-    public void Assign(GameObject rank, GameObject name, GameObject score)
-    {
-        Score = score;
-        Name = name;
-        Rank = rank;
-    }
-
     public void SetScore(string rank, string name, string score)
     {
         
