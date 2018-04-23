43dc69c396210ccc1a68f547c0df4d10f28b4025
Adawesome <adam.ccmpbll@gmail.com>
2018-04-22 23:47:35 -0700

    Added Pause Scene
    ::Created Room
    ::Canvas and Panel
    ::Themed Buttons
    ::Script to control buttons

---
+++ VRcade/Assets/Pause Scene/ButtonTextHighlight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System;
+
+public class ButtonTextHighlight : MonoBehaviour, IPointerEnterHandler, ISelectHandler
+{
+    public Button highlightedButton;
+    public Text buttonText;
+    public int scene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() { 
+	}
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonText.color = Color.black;
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        LoadScene(scene);
+    }
+    public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+}
