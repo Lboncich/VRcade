37f66a997c4fb5bde2217d4100188afa18a68291
Adawesome <adam.ccmpbll@gmail.com>
2018-04-23 20:49:49 -0700

    Pause Scene & Ball reset
    ultra omega

--- VRcade/Assets/Scripts/BowlingScripts/BowlingBall.cs
+++ VRcade/Assets/Scripts/BowlingScripts/BowlingBall.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class BowlingBall : MonoBehaviour {
     public Vector3 startPosition; //Keep track of where bowling ball starts
-                                   // Use this for initialization
-    void Start () {
+                                  // Use this for initialization
+    void Start() {
         startPosition = transform.position; // Saves position
-    }   
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (transform.position.y < -5f) // if too low
         {
             Debug.Log("falling");
             transform.position = startPosition;
             GetComponent<Rigidbody>().velocity = Vector3.zero; // resets velocity if moving
         }
+
     }
+
+   public void reset()
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        transform.position = startPosition;
+    }
+        
+
 }
--- VRcade/Assets/Scripts/BowlingScripts/RegionTest.cs
+++ VRcade/Assets/Scripts/BowlingScripts/RegionTest.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
-    //private Bowling fullGame;
-    private GameObject bowlingball;
+public class RegionTest : MonoBehaviour {
+    public GameObject bowlingball;
     public GameObject region;
 
     private bool isLegal;
@@ -14,22 +12,19 @@ public class GameManager : MonoBehaviour {
     {
         get; private set;
     }
-    // Use this for initialization
-    void Start () {
-       // fullGame = new global::Bowling();
-       // fullGame.startFirstFrame();
+	// Use this for initialization
+	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        bowlingball = GameObject.Find("Bowling_Ball");
         if (bowlingball.transform.position.z < region.transform.position.z)//bowling ball is in legal boundary
         {
             IsLegal = true;
-            
         }
         else
             IsLegal = false;
-        Debug.Log("Value of legal: "+IsLegal);
     }
+    
 }
