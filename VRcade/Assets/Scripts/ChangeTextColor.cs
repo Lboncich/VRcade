19cc9fceb63a22aeedfa312ed8e4155aa77d917a
Adawesome <adam.ccmpbll@gmail.com>
2018-04-27 21:42:14 -0700

    Pause Buttons Function
    -Added basic functionality to the 4 buttons
    -Created Settings-Panel (same canvas)
    -Added basic Setting and Button function
    -Main Menu: Loads scene 0
    -Terminate: Kills application
    -Settings: Loads Settings panel, deactivates Pause panel
    -Restart: Reloads scene 1
    --> UNTESTED (dont see why it wouldnt work tho)

---
+++ VRcade/Assets/Scripts/ChangeTextColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChangeTextColor : MonoBehaviour {
+    public Text colorChange;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        colorChange.color = new Color32(0, 0, 0, 255);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        colorChange.color = new Color32(27, 118, 31, 255);
+    }
+}
