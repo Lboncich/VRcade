8da222ca6b6bfe7d9c621f46eff28dc0e34584d0
Adawesome <adam.ccmpbll@gmail.com>
2018-04-29 15:51:20 -0700

    Pause Game and Pause Menu
    1: Pause Canvas
    1.1:Restart Game (reloads scene from scratch)
    1.2:Settings (brings up new settings panel)
    1.3:Quit App (kills the application)
    1.4: Main Menu (loads Menu Scene)
    Script: Change text highlight
    --With a given text box, when pointed too, the text changes colors
    EventListener addition
    --added delegate function?
    --->when button 2 pressed, pause game and bring up pause menu --->when
    pressed again, unpauses game and kills the pause menu

--- VRcade/Assets/BowlingAssets/EventListener.cs
+++ VRcade/Assets/BowlingAssets/EventListener.cs
@@ -9,6 +9,7 @@ public class EventListener : MonoBehaviour
     {
 
     public BowlingBall bb;
+    public GameObject pauseCanvas;
     //public List<GameObject> bbs;
     //System.Random rnd = new System.Random();
 
@@ -22,30 +23,36 @@ public class EventListener : MonoBehaviour
         }
         GetComponent<VRTK_ControllerEvents>().ButtonOneTouchStart += new ControllerInteractionEventHandler(DoButtonOneTouchStart);
         GetComponent<VRTK_ControllerEvents>().ButtonOneTouchEnd += new ControllerInteractionEventHandler(DoButtonOneTouchEnd);
-
+        GetComponent<VRTK_ControllerEvents>().ButtonTwoPressed += new ControllerInteractionEventHandler(DoButtonTwoTouched);
 
     }
-        private void DoButtonOneTouchStart(object sender, ControllerInteractionEventArgs e)
+    private void DoButtonTwoTouched(object sender, ControllerInteractionEventArgs e)
+    {
+        if (Time.timeScale == 1)
         {
-
-           
-            //GameObject ball = GameObject.FindGameObjectWithTag("BowlingBall");
-            //int rndint= rnd.Next(0, 2);
-            //Debug.Log(rndint);
+            Time.timeScale = 0;
+            pauseCanvas.SetActive(true);
+        }else if(Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            pauseCanvas.SetActive(false);
+        }
+    }
+    private void DoButtonOneTouchStart(object sender, ControllerInteractionEventArgs e)
+    { 
+        //GameObject ball = GameObject.FindGameObjectWithTag("BowlingBall");
+        //int rndint= rnd.Next(0, 2);
+        //Debug.Log(rndint);
         //GameObject randomBB = bbs[rndint];
         //bb = bbs[rndint];
         //Debug.Log("Where");
         //bb.Reset();
-
-
-
-           // DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "BUTTON ONE", "touched", e);
-        }
-
-        private void DoButtonOneTouchEnd(object sender, ControllerInteractionEventArgs e)
-        {
-            //DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "BUTTON ONE", "untouched", e);
-        }
+        // DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "BUTTON ONE", "touched", e);
+    }
+    private void DoButtonOneTouchEnd(object sender, ControllerInteractionEventArgs e)
+    {
+        //DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "BUTTON ONE", "untouched", e);
+    }
     private void DebugLogger(uint index, string button, string action, ControllerInteractionEventArgs e)
     {
         VRTK_Logger.Info("Controller on index '" + index + "' " + button + " has been " + action
---
+++ VRcade/Assets/Scripts/ChangeTextOnHighlight.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChangeTextOnHighlight : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public Text textToChange;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        textToChange.color = new Color32(0, 0, 0, 255);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        textToChange.color = new Color32(27, 118, 31, 255);
+    }
+}
